6cafc5de483058757d48cf60e5f9a0865f7261d6
Aaron Banks <aaronbanks@aaronbanks.ca>
2021-08-20 01:08:13 +0000

    Because it's my website

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import logo from './logo.svg';
+import logo from './bush.png';
 
 import './App.css';
 
 const App = () => {
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  let content;
+  if (!loggedIn) {
+    content = <div>
+    <img src={logo} className="App-logo" />
+      <p>
+      <form onSubmit={event => {
+        if (password === "kayla") {
+          setLoggedIn(true);
+        } else {
+          alert("fuck off");
+        }
+          
+        event.preventDefault();
+      }}>
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter password here!"
+          onChange={event => {
+            setPassword(event.target.value);
+          }}
+        />
+        <button type="submit">
+          Login!
+        </button>
+      </form>
+      </p>
+    </div>
+    } else {
+      content = <div>
+        Hi
+      </div>
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org">
-            Learn React
-          </a>
-        </p>
+        {content}
       </header>
     </div>
   );

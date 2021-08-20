8cdbadb2ff026498e567dff5efe6d0d029389aee
aaronbanks <aaronbanks@users.noreply.github.com>
2021-08-20 15:59:52 +0000

    deploy: 5f36128b3f3def110f1e2fd2ca81e3ac5eccb9fa

--- dist/App.js
+++ dist/App.js
@@ -1,18 +1,19 @@
 import React, {useState} from "../_snowpack/pkg/react.js";
+import CanoePage from "./CanoePage.js";
 import logo from "./face.png.proxy.js";
 import "./App.css.proxy.js";
 const App = () => {
   const [password, setPassword] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
   let content;
-  if (!loggedIn) {
+  if (currentPage === "login") {
     content = /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("img", {
       src: logo,
       className: "App-logo"
     }), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("form", {
       onSubmit: (event) => {
         if (password === "kayla") {
-          setLoggedIn(true);
+          setCurrentPage("intro");
         } else {
           alert("fuck off");
         }
@@ -28,12 +29,27 @@ const App = () => {
     }), /* @__PURE__ */ React.createElement("button", {
       type: "submit"
     }, "Login!"))));
+  } else if (currentPage === "intro") {
+    content = /* @__PURE__ */ React.createElement("div", {
+      className: "mainPage"
+    }, /* @__PURE__ */ React.createElement("p", {
+      id: "IntroParagraph"
+    }, "This is my first paragraph ", /* @__PURE__ */ React.createElement("br", null), "this is the next line of the paragraph ", /* @__PURE__ */ React.createElement("br", null), "and so on..... ", /* @__PURE__ */ React.createElement("br", null)), /* @__PURE__ */ React.createElement("p", {
+      id: "CanoeBio",
+      onClick: () => {
+        setCurrentPage("canoe");
+      }
+    }, "This paragraph summarizes my canoeing and canoe guide experiences ", /* @__PURE__ */ React.createElement("br", null), "when you click on the paragraph, something should happen ", /* @__PURE__ */ React.createElement("br", null), "maybe it expands in to a new page?! ", /* @__PURE__ */ React.createElement("br", null)), /* @__PURE__ */ React.createElement("p", {
+      id: "SnowboardingBio"
+    }, "This paragraph summarizes my canoeing and canoe guide experiences", /* @__PURE__ */ React.createElement("br", null), "maybe it expands in to a new page?! ", /* @__PURE__ */ React.createElement("br", null)));
+  } else if (currentPage === "canoe") {
+    content = /* @__PURE__ */ React.createElement(CanoePage, null);
   } else {
-    content = /* @__PURE__ */ React.createElement("div", null, "Hi");
+    console.error("what the hell");
   }
   return /* @__PURE__ */ React.createElement("div", {
     className: "App"
-  }, /* @__PURE__ */ React.createElement("header", {
+  }, /* @__PURE__ */ React.createElement(CanoePage, null), /* @__PURE__ */ React.createElement("header", {
     className: "App-header"
   }, content));
 };
---
+++ dist/CanoePage.js
@@ -0,0 +1,5 @@
+import React from "../_snowpack/pkg/react.js";
+const CanoePage = () => {
+  return /* @__PURE__ */ React.createElement("p", null, "I like canoes");
+};
+export default CanoePage;
